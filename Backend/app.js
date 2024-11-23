7d5065b23d8fc4470f5d8e6c6ccd5be05f00cc0b
00000921 <00000921@uca.edu.sv>
2024-11-23 16:12:10 -0600

    feat: cors

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,28 @@
 const express = require('express');  
+const cors = require('cors');  // Importar cors  
 const connectDB = require('./config/db');  
 const authRoutes = require('./routes/auth');  
 const userRoutes = require('./routes/user');  
 const jobApplicationRoutes = require('./routes/jobApplication');  
-const applyRoutes = require('./routes/apply'); 
+const applyRoutes = require('./routes/apply');   
 const cvRoutes = require('./routes/cv');  
 const feedbackRoutes = require('./routes/feedback');  
 
 const app = express();  
 connectDB();  
 
+// Configurar CORS  
+app.use(cors({  
+    origin: 'http://localhost:3000', // Cambia esto por el dominio de tu frontend  
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Métodos permitidos  
+    allowedHeaders: ['Content-Type', 'Authorization'] // Encabezados permitidos  
+}));  
+
 app.use(express.json());  
 app.use('/api/auth', authRoutes);  
 app.use('/api/user', userRoutes);  
 app.use('/api/job-application', jobApplicationRoutes);  
-app.use('/api/apply', applyRoutes); 
+app.use('/api/apply', applyRoutes);   
 app.use('/api/cv', cvRoutes);  
 app.use('/api/feedback', feedbackRoutes);  
 
