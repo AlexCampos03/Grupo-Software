48e1435b510ba56021b2343afbc83fd7ca7ceffb
00000921 <00000921@uca.edu.sv>
2024-11-23 11:59:50 -0600

    feat: autentication roles

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -7,9 +7,15 @@ exports.authenticate = async (req, res, next) => {
 
     try {  
         const decoded = jwt.verify(token, process.env.JWT_SECRET);  
-        req.user = await User.findById(decoded.id);  
+        const user = await User.findById(decoded.id);  
+        
+        if (!user) {  
+            return res.status(404).json({ message: 'Usuario no encontrado' });  
+        }  
+
+        req.user = user; // Guardar el usuario en req.user  
         next();  
     } catch (error) {  
-        return res.status(401).json({ message: 'Unauthorized' });  
+        return res.status(401).json({ message: 'Unauthorized', error: error.message });  
     }  
 };
