c512243af720de98002e3931e7232c442eb1e064
00000921 <00000921@uca.edu.sv>
2024-11-23 11:41:59 -0600

    feat: controllers apply

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');  
 const userRoutes = require('./routes/user');  
 const jobApplicationRoutes = require('./routes/jobApplication');  
+const applyRoutes = require('./routes/apply'); 
 const cvRoutes = require('./routes/cv');  
 const feedbackRoutes = require('./routes/feedback');  
 
@@ -12,7 +13,8 @@ connectDB();
 app.use(express.json());  
 app.use('/api/auth', authRoutes);  
 app.use('/api/user', userRoutes);  
-app.use('/api/job-application', jobApplicationRoutes); 
+app.use('/api/job-application', jobApplicationRoutes);  
+app.use('/api/apply', applyRoutes); 
 app.use('/api/cv', cvRoutes);  
 app.use('/api/feedback', feedbackRoutes);  
 
